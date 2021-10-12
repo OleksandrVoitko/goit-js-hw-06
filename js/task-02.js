1ff4c5e0eeb77cff377f83549fd08ce6f18dc993
OleksandrVoitko <voytkomail@gmail.com>
2021-10-12 11:17:04 +0300

    Задание 02 - готово

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const allLiIngredients = ingredients.map((element) => {
+  const liItem = document.createElement("li");
+  liItem.textContent = element;
+  liItem.classList.add("item");
+  return liItem;
+});
+
+const ulIngredientsRef = document.querySelector("#ingredients");
+
+ulIngredientsRef.append(...allLiIngredients);
