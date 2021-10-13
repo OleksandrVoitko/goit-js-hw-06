83ea4c22eca8ce7ae35354ac2426c1fe32c3c7df
OleksandrVoitko <voytkomail@gmail.com>
2021-10-13 22:10:49 +0300

    Задание 06 - готово.

--- js/task-06.js
+++ js/task-06.js
@@ -6,7 +6,7 @@ function onInputBlur(event) {
   if (inputRef.classList.contains("valid")) inputRef.classList.remove("valid");
   else inputRef.classList.remove("invalid");
 
-  if (event.currentTarget.value.length === Number(inputRef.dataset.length))
+  if (event.currentTarget.value.length === Number(event.currentTarget.dataset.length))
     inputRef.classList.add("valid");
   else inputRef.classList.add("invalid");
 }
