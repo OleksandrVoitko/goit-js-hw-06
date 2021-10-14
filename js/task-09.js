efd495fbd9aa6794f852c51f7752125bfee71172
OleksandrVoitko <voytkomail@gmail.com>
2021-10-14 13:08:56 +0300

    Задание 09 - готово

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.body;
+const changeColorBtnRef = document.querySelector(".change-color");
+const spanBackgroundColorRef = document.querySelector(".color");
+
+changeColorBtnRef.addEventListener("click", onColorBtnClick);
+
+function onColorBtnClick() {
+  const newBackgroundColor = getRandomHexColor();
+  
+  bodyRef.style.background = newBackgroundColor;
+  spanBackgroundColorRef.textContent = newBackgroundColor;
+}
