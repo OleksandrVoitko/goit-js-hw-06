74605ccf29ac269d21076bd1b5090de893e86945
OleksandrVoitko <voytkomail@gmail.com>
2021-10-14 12:48:08 +0300

    Задание 08 - готово

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginFormRef = document.querySelector(".login-form");
 
+loginFormRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  if (
+    formElements.email.value.length === 0 ||
+    formElements.password.value.length === 0
+  )
+    return alert("Все поля формы должны быть заполнены!");
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  const formData = { mail, password };
+
+  loginFormRef.reset();
+
+  return console.log(formData);
+}
